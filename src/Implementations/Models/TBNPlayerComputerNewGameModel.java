76c8de87a6ee0e3e7ed1f015024538ecb3813a36
dziku <fenix_91@go2.pl>
2015-06-02 11:51:12 +0200

    Dodałem gracza z animacją (Blender 2.63)

--- src/Implementations/Models/TBNPlayerComputerNewGameModel.java
+++ src/Implementations/Models/TBNPlayerComputerNewGameModel.java
@@ -1,7 +1,14 @@
 package Implementations.Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.AnimEventListener;
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.asset.AssetManager;
 import com.jme3.bullet.BulletAppState;
@@ -14,14 +21,22 @@ import com.jme3.input.InputManager;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
 import com.jme3.renderer.Camera;
+import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.plugins.blender.BlenderModelLoader;
+import com.jme3.shadow.DirectionalLightShadowFilter;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
 
 import Implementations.Controllers.TBNPlayerComputerNewGameController;
+import Implementations.Others.IO;
 
-public class TBNPlayerComputerNewGameModel implements ActionListener {
+public class TBNPlayerComputerNewGameModel extends AbstractAppState implements ActionListener, AnimEventListener {
 
 	private SimpleApplication app;
 	private Node rootNode;
@@ -42,6 +57,9 @@ public class TBNPlayerComputerNewGameModel implements ActionListener {
 	private boolean down;
 	private FlyByCamera flyCam;
 	private Camera cam;
+	private AnimControl animationControl;
+	private AnimChannel animationChannel;
+	private ViewPort viewPort;
 
 	public TBNPlayerComputerNewGameModel(Application app,
 			TBNPlayerComputerNewGameController tbnPlayerComputerNewGameController) {
@@ -52,6 +70,7 @@ public class TBNPlayerComputerNewGameModel implements ActionListener {
 		inputManager = this.app.getInputManager();
 		flyCam = this.app.getFlyByCamera();
 		cam = this.app.getCamera();
+		viewPort = this.app.getViewPort();
 		this.tbnPlayerComputerNewGameController = tbnPlayerComputerNewGameController;
 		bulletAppState = new BulletAppState();
 		stateManager.attach(bulletAppState);
@@ -59,21 +78,29 @@ public class TBNPlayerComputerNewGameModel implements ActionListener {
 	}
 
 	public void loadGame() {
-//		PhysicsTestHelper.createPhysicsTestWorld(rootNode, assetManager,
-//				bulletAppState.getPhysicsSpace());
+		
+		DirectionalLight sun = new DirectionalLight();
+        sun.setColor(ColorRGBA.White);
+        sun.setDirection(cam.getDirection());
+        rootNode.addLight(sun);
 		
 		assetManager.registerLoader(BlenderModelLoader.class, "blend");
-		ring = (Node) assetManager.loadModel("Assets/Models/ring/ringBezBokow.blend");
+		ring = (Node) assetManager.loadModel("Assets/Models/ring/ring.blend");
 		ring.setName("ring");
 		rootNode.attachChild(ring);
 		RigidBodyControl phy = new RigidBodyControl(0f);
 		ring.addControl(phy);
 		bulletAppState.getPhysicsSpace().add(phy);
-		
+
 		cam.setLocation(new Vector3f(0f, 5f, 20f));
 		
+//		TODO: wersja dla ogre
+//		player1 = (Node) assetManager.loadModel("Assets/Models/playerOgre/Genesis2Male-geom.mesh.xml");
+		
+//		TODO: wersja dla blendera
 		player1 = (Node) assetManager.loadModel("Assets/Models/player/player.blend");
-		player1.setLocalScale(3f);
+		
+		player1.setLocalScale(0.03f);
 		rootNode.attachChild(player1);
 		CapsuleCollisionShape capsule = new CapsuleCollisionShape(1f, 1f);
 		character = new BetterCharacterControl(0.3f, 2.5f, 8f);
@@ -81,15 +108,29 @@ public class TBNPlayerComputerNewGameModel implements ActionListener {
 		player1.addControl(character);
 		bulletAppState.getPhysicsSpace().add(character);
 		
+//		TODO: wersja dla ogre
+//		animationControl = player1.getControl(AnimControl.class);
 		
+//		TODO: wersja dla blendera
+		Spatial node1 = player1.getChild("RootNode");
+		Spatial node2 = player1.getChild("Genesis2Male-skinInstance");
+		IO.printL(node2);
+		animationControl = node2.getControl(AnimControl.class);
+		
+		IO.printL(animationControl);
+		
+		animationControl.addListener(this);
+		animationChannel = animationControl.createChannel();
+		List animations = new ArrayList(animationControl.getAnimationNames());
+		IO.printL(animations);
+//		animationChannel.setAnim("stay");
+
 		inputManager.addMapping("CharLeft", new KeyTrigger(KeyInput.KEY_A));
 		inputManager.addMapping("CharRight", new KeyTrigger(KeyInput.KEY_D));
 		inputManager.addMapping("CharForward", new KeyTrigger(KeyInput.KEY_W));
 		inputManager.addMapping("CharBackward", new KeyTrigger(KeyInput.KEY_S));
-		inputManager
-				.addMapping("CharJump", new KeyTrigger(KeyInput.KEY_RETURN));
-		inputManager.addMapping("CharAttack",
-				new KeyTrigger(KeyInput.KEY_SPACE));
+		inputManager.addMapping("CharJump", new KeyTrigger(KeyInput.KEY_RETURN));
+		inputManager.addMapping("CharAttack", new KeyTrigger(KeyInput.KEY_SPACE));
 		inputManager.addListener(this, "CharLeft", "CharRight");
 		inputManager.addListener(this, "CharForward", "CharBackward");
 		inputManager.addListener(this, "CharJump", "CharAttack");
@@ -104,7 +145,7 @@ public class TBNPlayerComputerNewGameModel implements ActionListener {
 				left = false;
 		} else if (binding.equals("CharRight")) {
 			if (value)
-				right = true;	
+				right = true;
 			else
 				right = false;
 		} else if (binding.equals("CharForward")) {
@@ -119,10 +160,20 @@ public class TBNPlayerComputerNewGameModel implements ActionListener {
 				down = false;
 		} else if (binding.equals("CharJump"))
 			character.jump();
-		
+
 		if (binding.equals("CharAttack")) {
 			character.jump();
 		}
+	}
+
+	@Override
+	public void onAnimChange(AnimControl arg0, AnimChannel arg1, String arg2) {
+
+	}
+
+	@Override
+	public void onAnimCycleDone(AnimControl arg0, AnimChannel arg1, String arg2) {
+
 	}
 
 }
---
+++ src/Implementations/Players/MyPlayer.java
@@ -0,0 +1,7 @@
+package Implementations.Players;
+
+import Interfaces.Player;
+
+public final class MyPlayer implements Player {
+
+}
